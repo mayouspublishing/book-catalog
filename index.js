ec62b6e95c5bc6fb5c7f4168b4ed389f7a235268
mayouspublishing <admin@mayous.org>
2025-03-30 22:05:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,40 @@
 const TARGET_BASE = "https://script.google.com/macros/s/AKfycbyIcapwCMZq0w9myM1PlNEuT39VAGPaBy277B2ZSkhp6mRgV08Bsa85t3k7NDV8id0a/exec";
 
-class ResourceRewriter {
-  element(element) {
-    const attributes = ["href", "src", "action"];
-
-    for (const attr of attributes) {
-      const val = element.getAttribute(attr);
-
-      if (val && val.startsWith("/")) {
-        element.setAttribute(attr, TARGET_BASE + val);
-      }
-    }
-  }
-}
-
-async function handleRequest(request) {
-  const url = new URL(request.url);
-  const path = url.pathname.replace(/^\/shop/, "") + url.search;
-  const targetUrl = TARGET_BASE + path;
-
-  const response = await fetch(targetUrl, {
-    method: request.method,
-    headers: {
-      ...Object.fromEntries(request.headers),
-      cookie: null,
-    },
-    body: request.body,
-    redirect: "manual",
-  });
-
-  const contentType = response.headers.get("content-type") || "";
-
-  if (contentType.includes("text/html")) {
-    return new HTMLRewriter()
-      .on("a", new ResourceRewriter())
-      .on("form", new ResourceRewriter())
-      .on("img", new ResourceRewriter())
-      .on("link", new ResourceRewriter())
-      .on("script", new ResourceRewriter())
-      .on("iframe", new ResourceRewriter())
-      .transform(response);
-  }
-
-  return response;
-}
-
 export default {
-  fetch(request, env, ctx) {
-    return handleRequest(request);
+  async fetch(request) {
+    const html = `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <title>Mayous Book Catalog</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style>
+          html, body {
+            margin: 0;
+            padding: 0;
+            height: 100%;
+            width: 100%;
+          }
+          iframe {
+            border: none;
+            width: 100%;
+            height: 100%;
+          }
+        </style>
+      </head>
+      <body>
+        <iframe src="${TARGET_BASE}" allow="fullscreen"></iframe>
+      </body>
+      </html>
+    `;
+
+    return new Response(html, {
+      headers: {
+        "Content-Type": "text/html; charset=utf-8",
+        "X-Frame-Options": "ALLOWALL",  // Optional: allows iframe embedding
+      },
+    });
   },
 };
+
